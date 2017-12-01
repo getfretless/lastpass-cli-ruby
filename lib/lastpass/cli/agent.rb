5d17105a0fdfac086fb7c5a585b78fc6958fbfd8
David Jones <unixmonkey1@gmail.com>
2017-12-01 11:40:51 -0500

    Add agent class to handle login/logout
    - Make command methods return command only, without executing.
    - Add class-level Command.run
    - Use capture2e for run
    - Add default configuration parameter for executable
    - Prevent login from running when already logged-in
    - Prevent logout from running when already logged-out

---
+++ lib/lastpass/cli/agent.rb
@@ -0,0 +1,32 @@
+module Lastpass
+  module CLI
+    class Agent
+      attr_reader :logged_in, :config
+
+      def initialize
+        @config = Lastpass::CLI.configuration
+        @logged_in = false
+      end
+
+      def login
+        return true if @logged_in
+        command = [config.executable]
+        command += Command.new.login(username: config.username)
+        out, _, _ = Open3.capture2e(
+          { 'LPASS_DISABLE_PINENTRY' => '1' },
+          *command,
+          stdin_data: "#{config.password}\n"
+        )
+        @logged_in = !!out.match('Success: Logged in')
+      end
+
+      def logout
+        return true if !@logged_in
+        command = [config.executable]
+        command += Command.new.logout
+        out, _, _ = Open3.capture2e(*command)
+        @logged_in = !!out.match('Log out: complete')
+      end
+    end
+  end
+end
--- lib/lastpass/cli/configuration.rb
+++ lib/lastpass/cli/configuration.rb
@@ -1,7 +1,11 @@
 module Lastpass
   module CLI
     class Configuration
-      attr_accessor :username, :password
+      attr_accessor :username, :password, :executable
+
+      def initialize
+        @executable = 'lpass'
+      end
     end
   end
 end
